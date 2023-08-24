bcfa144f79c4aa87512bf1bb22a0f12a78fdf3ec
NafisAshraf <nafis.ashraf.roudra@g.bracu.ac.bd>
2023-08-24 14:54:27 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,1 @@
+# Bored-Tunnelers
---
+++ boredtunnelers/src/App.js
@@ -0,0 +1,32 @@
+import React from "react"; 
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </React.Fragment>
+  );
+}
+
+export default App;
