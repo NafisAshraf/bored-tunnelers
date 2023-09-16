ecc7527e5cfd655b89d16095ee39b6ffd168ccf9
NafisAshraf <nafis.ashraf.roudra@g.bracu.ac.bd>
2023-09-16 10:00:02 +0600

    updated some information in front page

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,116 +1,127 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
     <>
-      <div class="container my-5">
-        <div class="p-5 text-center bg-body-tertiary rounded-3">
-          <img src="images/logo.png" alt="logo" class="bi mt-2 mb-5" width="250" height="250"/>
-          <h1 class="text-body-emphasis">Bored Tunnelers</h1>
-          <p class="col-lg-8 mx-auto fs-5 text-muted">
-            This is a custom jumbotron featuring an image at the top, some
-            longer text that wraps early thanks to a responsive
-            <code>.col-*</code> class, and a customized call to action.
+      <div className="container my-5">
+        <div className="p-5 text-center bg-body-tertiary rounded-3">
+          <img src="images/logo.png" alt="logo" className="bi mt-2 " width="250" height="250"/>
+          <h1 className="display-5 fw-bold py-3">Bored Tunnelers</h1>
+          <p className="col-lg-8 mx-auto fs-5 text-muted lh-lg">
+          Redefining the future of tunneling technology as 
+          the first ever team from Bangladesh participating 
+          in Elon Musk's Not-a-Boring competition 2024.
+          We are a dynamic team of tunneling enthusiasts 
+          who are passionate about pushing the boundaries 
+          of what's possible underground. With a shared 
+          dedication to innovation, expertise, and teamwork, 
+          we are committed to revolutionizing the tunneling 
+          industry and making a lasting impact.
           </p>
-          <div class="d-inline-flex gap-2 mb-5">
+          {/* <div className="d-inline-flex gap-2 mb-5">
             <button
-              class="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill"
+              className="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill"
               type="button"
             >
               Call to action
             </button>
             <button
-              class="btn btn-outline-secondary btn-lg px-4 rounded-pill"
+              className="btn btn-outline-secondary btn-lg px-4 rounded-pill"
               type="button"
             >
               Secondary link
             </button>
+          </div> */}
+        </div>
+      </div>
+
+      <div className="b-example-divider"></div>
+
+      <div className="my-5">
+        <div className="p-5 text-center bg-body-tertiary">
+          <div className="container py-5">
+            <h1 className="text-body-emphasis">Our Team</h1>
+            {/* <h1 className="display-5 fw-bold py-3">Our Team</h1> */}
+            <p className="col-lg-8 mx-auto lead lh-lg">
+            Our diverse team brings together talented engineers, architects, geologists, and tunneling experts who have honed their skills through years of experience. Together, we possess a deep understanding of underground construction, drilling technologies, and sustainable tunneling methods.
+            </p>
+            <Link to="/team">
+              <button className="btn btn-outline-secondary px-5 mb-5" type="button">
+                Team Members
+              </button>
+            </Link>
+            
           </div>
         </div>
       </div>
 
-      <div class="b-example-divider"></div>
+      <div className="b-example-divider"></div>
 
-      <div class="container my-5">
-        <div class="position-relative p-5 text-center text-muted bg-body border border-dashed rounded-5">
+      <div className="container my-5">
+        <div className="position-relative p-5 text-center text-muted bg-body border border-dashed rounded-5">
           <button
             type="button"
-            class="position-absolute top-0 end-0 p-3 m-3 btn-close bg-secondary bg-opacity-10 rounded-pill"
+            className="position-absolute top-0 end-0 p-3 m-3 btn-close bg-secondary bg-opacity-10 rounded-pill"
             aria-label="Close"
           ></button>
-          <img src="images/logo.png" alt="logo" class="bi mt-2 mb-5" width="250" height="250"/>
-          <h1 class="text-body-emphasis">Placeholder jumbotron</h1>
-          <p class="col-lg-6 mx-auto mb-4">
+          <img src="images/logo.png" alt="logo" className="bi mt-2 mb-5" width="250" height="250"/>
+          <h1 className="text-body-emphasis">Placeholder jumbotron</h1>
+          <p className="col-lg-6 mx-auto mb-4">
             This faded back jumbotron is useful for placeholder content. It's
             also a great way to add a bit of context to a page or section when
             no content is available and to encourage visitors to take a specific
             action.
           </p>
-          <button class="btn btn-primary px-5 mb-5" type="button">
+          <button className="btn btn-primary px-5 mb-5" type="button">
             Call to action
           </button>
         </div>
       </div>
 
-      <div class="b-example-divider"></div>
+      <div className="b-example-divider"></div>
 
-      <div class="my-5">
-        <div class="p-5 text-center bg-body-tertiary">
-          <div class="container py-5">
-            <h1 class="text-body-emphasis">Full-width jumbotron</h1>
-            <p class="col-lg-8 mx-auto lead">
-              This takes the basic jumbotron above and makes its background
-              edge-to-edge with a <code>.container</code> inside to align
-              content. Similar to above, it's been recreated with built-in grid
-              and utility classes.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="b-example-divider"></div>
-
-      <div class="container my-5">
-        <div class="p-5 text-center bg-body-tertiary rounded-3">
-          <h1 class="text-body-emphasis">Basic jumbotron</h1>
-          <p class="lead">
+      <div className="container my-5">
+        <div className="p-5 text-center bg-body-tertiary rounded-3">
+          <h1 className="text-body-emphasis">Basic jumbotron</h1>
+          <p className="lead">
             This is a simple Bootstrap jumbotron that sits within a
-            <code>.container</code>, recreated with built-in utility classes.
+            <code>.container</code>, recreated with built-in utility classNamees.
           </p>
         </div>
       </div>
-      <div class="container py-4">
-        <div class="p-5 mb-4 bg-body-tertiary rounded-3">
-          <div class="container-fluid py-5">
-            <h1 class="display-5 fw-bold">Custom jumbotron</h1>
-            <p class="col-md-8 fs-4">
+      <div className="container py-4">
+        <div className="p-5 mb-4 bg-body-tertiary rounded-3">
+          <div className="container-fluid py-5">
+            <h1 className="display-5 fw-bold">Custom jumbotron</h1>
+            <p className="col-md-8 fs-4">
               Using a series of utilities, you can create this jumbotron, just
               like the one in previous versions of Bootstrap. Check out the
               examples below for how you can remix and restyle it to your
               liking.
             </p>
-            <button class="btn btn-primary btn-lg" type="button">
+            <button className="btn btn-primary btn-lg" type="button">
               Example button
             </button>
           </div>
         </div>
 
-        <div class="row align-items-md-stretch">
-          <div class="col-md-6">
-            <div class="h-100 p-5 text-bg-dark rounded-3">
+        <div className="row align-items-md-stretch">
+          <div className="col-md-6">
+            <div className="h-100 p-5 text-bg-dark rounded-3">
               <h2>Change the background</h2>
               <p>
                 Swap the background-color utility and add a `.text-*` color
                 utility to mix up the jumbotron look. Then, mix and match with
                 additional component themes and more.
               </p>
-              <button class="btn btn-outline-light" type="button">
+              <button className="btn btn-outline-light" type="button">
                 Example button
               </button>
             </div>
           </div>
-          <div class="col-md-6">
-            <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+          <div className="col-md-6">
+            <div className="h-100 p-5 bg-body-tertiary border rounded-3">
               <h2>Add borders</h2>
               <p>
                 Or, keep it light and add a border for some added definition to
@@ -118,14 +129,14 @@ const Homepage = () => {
                 at the source HTML here as we've adjusted the alignment and
                 sizing of both column's content for equal-height.
               </p>
-              <button class="btn btn-outline-secondary" type="button">
+              <button className="btn btn-outline-secondary" type="button">
                 Example button
               </button>
             </div>
           </div>
         </div>
 
-        <footer class="pt-3 mt-4 text-body-secondary border-top">
+        <footer className="pt-3 mt-4 text-body-secondary border-top">
           &copy; 2023
         </footer>
       </div>
