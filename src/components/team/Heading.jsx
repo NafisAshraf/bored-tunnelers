2481bf4208748811ce2e0352552408812e0d3c53
NafisAshraf <nafis.ashraf.roudra@g.bracu.ac.bd>
2023-12-10 00:39:59 +0600

    Update Heading.jsx

--- src/components/team/Heading.jsx
+++ src/components/team/Heading.jsx
@@ -3,7 +3,17 @@ import React from "react";
 function Heading(props) {
   return (
     <div className="my-5 pt-3 text-center">
-      <h1 className="display-5">{props.heading}</h1>
+      {/* <h1 className="display-5">{props.heading}</h1> */}
+      <h1
+        className=""
+        style={{
+          fontSize: 50,
+          fontWeight: 700,
+          fontFamily: "'Roboto', sans-serif",
+        }}
+      >
+        {props.heading}
+      </h1>
       <hr className=" mx-auto" style={{ width: "90%" }} />
     </div>
   );
